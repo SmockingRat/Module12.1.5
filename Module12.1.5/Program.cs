f5ce9df05a93dc9a2d96b1c42b1eab9249a78674
SmokingRat <halfbrikbarry@gmail.com>
2022-04-21 16:48:33 +0300

    Project fin

---
+++ Module12.1.5/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Module12._1._5
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Program main method
+        /// </summary>
+        /// <param name="args"> Entry parameter </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пользователь, введите свой Логин");
+            var user = new User();
+            string login = Console.ReadLine().ToLower();
+            if (!user.CheckLogin(login))
+            {
+                Console.Write("Отсутствует такой пользователь, создание нового");
+                Thread.Sleep(500);
+                Console.Write(".");
+                Thread.Sleep(500);
+                Console.Write(".");
+                Thread.Sleep(500);
+                Console.WriteLine(".");
+                user.AddUser(user);
+            }
+
+            try
+            {
+                var currentUser = user.Users[login];
+                if (currentUser.IsPremium)
+                {
+                    Console.WriteLine("Добро пожаловать, пользователь!");
+                }
+                else
+                {
+                    var mainClass = new Program();
+                    mainClass.ShowAdd();
+                    Console.WriteLine("Добро пожаловать, пользователь!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Такого Логина нет!");
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Method ShowAdd show add to user
+        /// </summary>
+        public void ShowAdd()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Module12.1.5/User.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12._1._5
+{
+    /// <summary>
+    /// Class describe User
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Dictionary contain Users
+        /// </summary>
+        public Dictionary<string, User> Users;
+
+        /// <summary>
+        /// Kinds of User
+        /// </summary>
+        public User()
+        {
+            Users = new Dictionary<string, User>();
+        }
+
+        /// <summary>
+        /// Login of User
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Name of User
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Have User premium account or not
+        /// </summary>
+        public bool IsPremium { get; set; }
+
+        /// <summary>
+        /// Method add new user in dictionary
+        /// </summary>
+        /// <param name="user"> Parameter from main function </param>
+        public void AddUser(User user)
+        {
+            string ans;
+            Console.WriteLine("Введите логин");
+            user.Login = Console.ReadLine().ToLower();
+            Console.WriteLine("Введите Имя");
+            user.Name = Console.ReadLine().ToLower();
+            bool exit = false;
+
+            do
+            {
+                Console.WriteLine("Хотите премиум-подписку?");
+                ans = Console.ReadLine().ToLower();
+                exit = CheckAnsw(ans);
+            } while (exit == false);
+
+            if(ans == "yes")
+            {
+                user.IsPremium = true;
+            }
+            else
+            {
+                user.IsPremium = false;
+            }
+
+                Users.Add(user.Login, user);
+        }
+
+        /// <summary>
+        /// Method CheckAnsw check answer to get out of cycle
+        /// </summary>
+        /// <param name="ans"> Parameter contain answer </param>
+        /// <returns> Return true or false, after checking answer </returns>
+        public bool CheckAnsw(string ans)
+        {
+            bool n = ans == "yes" | ans == "no";
+            return n;
+        }
+
+        /// <summary>
+        /// Method CheckLogin check dictionary with login      
+        /// </summary>
+        /// <param name="login"> Login, that have to be checked </param>
+        /// <returns> Return true or false, after checking the login </returns>
+        public bool CheckLogin(string login)
+        {
+            bool n;
+            try
+            {
+                n = Users.ContainsKey(login);
+            }
+            catch
+            {
+                n = false;
+            }
+
+            return n;
+        }
+    }
+}
